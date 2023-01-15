b4561758352a59e1c97a19f30a4bf2bd8186c26e
Marwan1998 <Dragonkingofhell@gmail.com>
2023-01-15 12:45:07 +0200

    added routes to auth group middleware, some refactoring

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,18 @@ Route::get('/', fn () => redirect('/home'));
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('currenciesInfos', App\Http\Controllers\currencies_infoController::class);
-
-Route::resource('currencies', App\Http\Controllers\CurrenciesController::class);
-
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('currenciesInfos', App\Http\Controllers\currencies_infoController::class);
+    Route::resource('currencies', App\Http\Controllers\CurrenciesController::class);
+});
 
 
 // Test Routes.
 Route::get('/hi', [App\Http\Controllers\TestController::class, 'index']);
 
-
-
-Route::group(['middleware' => ['role:master']], function () {
-
-    Route::get('other', function () {
-        return view('other.index');
-    })->name('other.index');
-
+Route::group(['middleware' => ['role:master', 'auth']], function () {
+    Route::get('other', fn () => view('other.index'))->name('other.index');
 });
 
 Route::group(['middleware' => ['role:master|Admin']], function () {
@@ -46,15 +39,11 @@ Route::group(['middleware' => ['role:master|Admin']], function () {
     Route::resource('roles', App\Http\Controllers\RoleController::class);
     Route::resource('users', App\Http\Controllers\UserController::class);
     Route::post('users/removeRole', [App\Http\Controllers\UserController::class, 'removeRole'])->name('users.removeRole');
-
 });
 
 
-Route::get('locale/{locale}', function ($locale){
+Route::get('locale/{locale}', function ($locale) {
     Session::put('locale', $locale);
     return redirect()->back();
 });
-
-
-
 
